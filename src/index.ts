f250713e23b7dbac46e03686f76a01d4478ea655
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2024-11-18 11:21:38 -0500

    fix env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Bot from './lib/bot';
 import generateField from './lib/generateField';
 
-const text = await Bot.run(generateField, { dryRun: true });
+const text = await Bot.run(generateField, { dryRun: false });
 
 console.log(`[${new Date().toISOString()}] Posted:\n${text}`);
