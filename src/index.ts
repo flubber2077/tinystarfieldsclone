753191501ce8e88b894dae215e2f2cc996ff034c
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2024-11-18 20:14:55 -0500

    bump and add extra post

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import dayjs from 'dayjs';
-import localizedFormat from 'dayjs/plugin/localizedFormat'
+import localizedFormat from 'dayjs/plugin/localizedFormat';
 import Bot from './lib/bot';
-import generateField from './lib/generateField'
+import generateField from './lib/generateField';
 
-dayjs.extend(localizedFormat)
+dayjs.extend(localizedFormat);
 
 const dryRun = process.env.NODE_ENV === 'development';
 const text = await Bot.run(generateField, { dryRun });
